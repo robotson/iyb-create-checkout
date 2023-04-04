c4caa99271ee904065468a0ba3531a406235ca64
robotson <robotsun@gmail.com>
2023-04-03 19:29:46 -0500

    hemmed and hawed about how best to managed my secret keys because dotenv-vault doesn't seem really to be the relevant move here but got stripe workin anyway

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+const Stripe = require('stripe');
+
+
+async function handleRequest(request, env) {
+    const stripe = Stripe(env.STRIPE_SECRET_KEY);
+
+    return new Response("http my face");
+}
+
+
 export default {
-  async fetch(request, env) {
-    return new Response("Hello world")
-  }
+  fetch: handleRequest
 }
